4cd7f3f0bfe84ef4eb096e81d11cbcafb1833dc8
l3ragio <48380612+l3ragio@users.noreply.github.com>
2025-07-24 17:14:52 +0200

    Update medium_about.rb

--- _plugins/medium_about.rb
+++ _plugins/medium_about.rb
@@ -1,22 +1,22 @@
 # _plugins/medium_about.rb
 require 'open-uri'
-require 'json'
-require 'jekyll'
+require 'nokogiri'
 
 Jekyll::Hooks.register :site, :post_read do |site|
   username = site.config['medium_username']
-  url = "https://medium.com/@#{username}/about?format=json"
-  raw = URI.open(url).read
-  clean = raw.gsub(/\A\)\]\}while\(1\);\s*<x>/, '')
-  data = JSON.parse(clean)
+  url = "https://medium.com/@#{username}/about"
+  html = URI.open(url).read
 
-  user = data.dig('payload', 'user') || {}
-  about_html = user['aboutMe'] || user['bio'] || ''
+  doc = Nokogiri::HTML(html)
 
-  site.data['medium_about'] = {
-    'name'        => user['name'],
-    'username'    => username,
-    'avatar_url'  => user['imageId'] ? "https://cdn-images-1.medium.com/fit/c/200/200/#{user['imageId']}" : nil
-  }
-  site.data['medium_about_html'] = about_html
+  # Select the actual rendered content in the <article> tag
+  article = doc.at('article')
+
+  if article
+    # Clean up unwanted scripts or attributes if needed
+    article.search('script, style, noscript').remove
+    site.data['medium_about_html'] = article.inner_html
+  else
+    site.data['medium_about_html'] = "<p>Unable to fetch Medium About content.</p>"
+  end
 end
